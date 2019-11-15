46f582b0cd9ea609199f87cbeaf0c6bc5cc56f4c
suemoto <s_bdash_0802@yahoo.co.jp>
2019-11-15 12:44:34 +0900

    add user_sign_up,sign_in,sign_out
    user　のサインアップ、サインイン、サインアウト昨日を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,14 @@ class UsersController < ApplicationController
   def edit
     
   end
-
   def update
-    
+
+    if current_user.update(user_params)
+      redirect_to root_path
+    else render :edit
+    end    
   end
+
+  def user_params
+    params.requier(:user).permit(:name, :email)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "messages#index"
+  resourses :users, only:[edit, update] do
+  end  
 end
