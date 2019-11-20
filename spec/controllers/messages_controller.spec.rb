639ee5b733ed5ba7b266f9554ee9a182d132989a
suemoto <s_bdash_0802@yahoo.co.jp>
2019-11-20 11:44:11 +0900

    set controller_test
    コントローラーのテストを追加

---
+++ spec/controllers/messages_controller.spec.rb
@@ -0,0 +1,91 @@
+require 'rails_helper'
+describe MessagesController do
+  let(:group) { create(:group) }
+  let(:user) { create(:user) }
+
+  describe "#index" do
+    context "ログインしている場合" do
+      before do
+        login user
+        get :index, params: { group_id: group.id }
+      end
+      
+      it 'assigns @message' do
+        expect(assigns(:message)).to be_a_new(Message)
+      end
+
+      it 'assigns @group' do
+        expect(assigns(:group)).to eq group
+      end
+
+      it "該当するビューが描画されているか" do
+        expect(response).to render_template :index
+      end
+    end
+
+    context "ログインしていない場合" do
+      before do
+        get :index, params: { group_id: group.id }
+      end
+
+      it 'redirects to new_user_session_path' do
+        expect(response).to redirect_to(new_user_session_path)
+      end
+      
+    end
+
+  end
+
+
+
+  describe "#create" do
+    let(:params) { {group_id: group.id, user_id: user.id, message: attributes_for(:message) } }
+
+    context "ログイン" do
+      before do
+        login user
+      end
+      context "保存に成功" do
+        subject {
+          post :create,
+          params: params
+        }
+        it "メッセージが保存できたか" do
+          expect { subject }.to change(Message, :count).by(1)  
+        end
+        it "意図した画面に遷移できたか" do
+          subject
+          expect(response).to redirect_to(group_messages_path(group))
+        end
+      end
+
+    
+      context "保存に失敗" do
+        let(:invalid_params) { { group_id: group.id, user_id: user.id, message: attributes_for(:message, content: nil, image: nil) } }
+        subject {
+            post :create,
+            params: invalid_params
+        }
+        
+        it "メッセージが保存できなかったか" do
+          expect{ subject }.not_to change(Message, :count)
+        
+        end
+        it "意図したビューが描画されているか" do
+          subject
+            expect(response).to render_template :index
+
+        end
+      end
+    end
+    context "ログインしていない" do
+      it 'redirects to new_user_session_path' do
+        post :create, params: params
+        expect(response).to redirect_to(new_user_session_path)
+ 
+      
+      end
+    end
+
+  end
+end
