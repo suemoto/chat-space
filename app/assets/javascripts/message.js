7939865f731e292f8321e632dae8332eac9790ec
suemoto <s_bdash_0802@yahoo.co.jp>
2019-11-21 12:52:09 +0900

    add_ajax_jbuilder
    ajaxの記述と、jbuilderでテータをjson型に変換するところを実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,2 +1,18 @@
 $(function(){
+  $('#new_message').on('submit', function(e){
+    e.preventDefault()
+
+    let formdata = new FormData(this);
+    let url = $(this).attr('action')
+
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formdata,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+
+  })
 });
---
+++ app/views/messages/create.json.jbuilder
@@ -0,0 +1,6 @@
+json.content @message.content
+json.group_id @message.group_id
+json.user_id @message.user_id
+json.created_at @message.created_at
+json.updated_at @message.updated_at
+binding.pry
