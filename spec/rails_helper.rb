faf0054a4d83ce74341feda9e9c00942e7a6b4c8
suemoto <s_bdash_0802@yahoo.co.jp>
2019-11-21 10:33:06 +0900

    Integration test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,4 +63,5 @@ RSpec.configure do |config|
   Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include ControllerMacros, type: :controller
+  require 'capybara/rspec'
 end
