8114dfaac3ae7b3af3110eb1820b6943c9a6bf8f
suemoto <s_bdash_0802@yahoo.co.jp>
2019-11-25 17:49:15 +0900

    auto_reload_complete
    自動更新機能完成

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,28 +1,30 @@
 
 $(function(){
-  last_message_id = $('.contents__message__content').last(1);
-  console.log(last_message_id);
+  // let last_message_id = $('.contents:last').data('message-id')
+  // console.log(last_message_id);
   function buildHTML(message){
-    let img = message.image? `<img class="lower-message__image" src="${message.image}" alt= "test image">`: "";
-    let content = message.content? `<p class="contents__message__content">${message.content}</p>`: "";
-    let html = `<div class="contents">
+    img = (message.image)? `<img class="lower-message__image" src=${message.image} >`: "";
+    // let content = message.content? `<p class="contents__message__content">${message.content}</p>`: "";
+    let html = `
+                <div class="contents" data-message-id= ${message.id}>
                   <div class="contents__block">
                     <div class="contents__block__user">
-                      ${message.user}
+                      ${message.user_name}
                     </div>
                     <div class="contents__block__date">
                       ${message.created_at}
                     </div>
                     </div>
                     <div class="contents__message">
-                      ${content}
+                    <p class="contents__message__content">
+                    ${message.content}
+                    </p>
+                     
                     </div>
                       ${img}
 
                   </div>
-                </div>`
-                
-                
+                </div>`           
     return html;
   }
   $('#new_message').on('submit', function(e){
@@ -50,6 +52,39 @@ $(function(){
     .fail(function() {
       alert("メッセージ送信に失敗しました");
   });
-
   })
+  var reloadMessages = function(message) {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    
+      last_message_id = $('.contents:last').data('message-id')
+      console.log(last_message_id)
+      group_id = $('.group-member__group').data('group-id')
+      $.ajax({
+        url: "api/messages",
+        type: 'get',
+        dataType: 'json',
+        data: {id: last_message_id}
+        // processData: false,
+        // contentType: false
+      })
+      .done(function(messages) {
+
+        console.log('success');
+        var insertHTML = '';
+        messages.forEach(function(message){
+          insertHTML = buildHTML(message); 
+          $('.chat').append(insertHTML)
+          $('.chat').animate({scrollTop: $('.chat')[0].scrollHeight}, 'fast');
+        })
+        
+        
+
+      })
+      
+      .fail(function() {         
+      });
+    }
+  };
+  setInterval(reloadMessages, 7000);
 });
